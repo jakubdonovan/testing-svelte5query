959db75c109d431baa2d3c8a5b493542ef962ca5
jakubdonovan <jakubdonovan@protonmail.com>
2024-07-27 20:11:21 +0700

    Refactored address component
    The address component has been refactored for better readability and functionality. The changes include:
    - Un-commented and implemented the 'selectAddress' function.
    - Added a state variable 'selectedAddress'.
    - Enhanced UI by adding conditional rendering based on the state of 'addressSuggestions' and 'selectedAddress'.
    - Removed commented out code to improve clarity.

--- src/lib/components/address.svelte
+++ src/lib/components/address.svelte
@@ -1,62 +1,48 @@
 <script lang="ts">
 	import { fetchAddressSuggestions } from '$lib/hooks/fetchAddressSuggestions.svelte';
-	// // 164 Ludlow St New York, NY, USA
+	// 164 Ludlow St New York, NY, USA
 	let address = $state('');
+	let selectedAddress = $state<string | null>(null);
+
 	let addressSuggestions = fetchAddressSuggestions(() => address);
+
+	async function selectAddress(index: number) {
+		if (!addressSuggestions.data) return;
+		selectedAddress = addressSuggestions.data[index];
+
+		return selectedAddress;
+	}
 
 	$inspect({
 		address,
 		isFetching: addressSuggestions.isFetching,
 		data: addressSuggestions.data
 	});
-
-	// let selectedAddress = $state('');
-	//
-	// async function selectAddress(index: number) {
-	// // when address selected from suggestions, suggestions should be cleared to de-render suggestions popup.
-	// 	if (!addressSuggestions.data) return;
-	// 	selectedAddress = addressSuggestions.data[index];
-	//
-	// 	return selectedAddress;
-	// }
-	//
-	//
-
-	// let address = '';
-	// const fetchAddressSuggestions = async (address: string): Promise<string[]> => {
-	// 	const res = await fetch(`/api/search-address?address=${address}`);
-	// 	if (!res.ok) throw new Error('Failed to fetch address suggestions');
-	//
-	// 	return await res.json();
-	// };
 </script>
 
-<input type="text" bind:value={address} placeholder="Enter delivery address" />
+<div>
+	<h2 class="text-xl font-medium">Enter your delivery address</h2>
+	{#if !selectedAddress}
+		<input type="text" bind:value={address} placeholder="Enter delivery address" />
+	{/if}
+	{#if addressSuggestions.isFetching || addressSuggestions.isPending}
+		Loading...
+	{/if}
+	{#if addressSuggestions.error}
+		An error has occurred: {addressSuggestions.error.message}
+	{/if}
+	{#if !selectedAddress && addressSuggestions.isSuccess && addressSuggestions.data.length}
+		<ul>
+			{#each addressSuggestions.data as suggestion, index}
+				<li><button onclick={() => selectAddress(index)}>{suggestion}</button></li>
+			{/each}
+		</ul>
+	{/if}
 
-<!-- <div> -->
-<!-- 	<h2 class="text-xl font-medium">Enter your delivery address</h2> -->
-<!-- 	{#if !selectedAddress} -->
-<!-- 		<input type="text" bind:value={address} placeholder="Enter delivery address" /> -->
-<!-- 	{/if} -->
-<!-- 	{#if addressSuggestions.isFetching || addressSuggestions.isPending} -->
-<!-- 		Loading... -->
-<!-- 	{/if} -->
-<!-- 	{#if addressSuggestions.error} -->
-<!-- 		An error has occurred: -->
-<!-- 		{addressSuggestions.error.message} -->
-<!-- 	{/if} -->
-<!-- 	{#if addressSuggestions.isSuccess} -->
-<!-- 		<ul> -->
-<!-- 			{#each addressSuggestions.data as suggestion, index} -->
-<!-- 				<li><button onclick={() => selectAddress(index)}>{suggestion}</button></li> -->
-<!-- 			{/each} -->
-<!-- 		</ul> -->
-<!-- 	{/if} -->
-<!---->
-<!-- 	{#if selectedAddress} -->
-<!-- 		<div> -->
-<!-- 			<p>{selectedAddress}</p> -->
-<!-- 			<button>change</button> -->
-<!-- 		</div> -->
-<!-- 	{/if} -->
-<!-- </div> -->
+	{#if selectedAddress}
+		<div>
+			<p>{selectedAddress}</p>
+			<button onclick={() => (selectedAddress = null)}>change</button>
+		</div>
+	{/if}
+</div>
