53c6c67885c7a1012976e5a945913aefcb902571
jakubdonovan <jakubdonovan@protonmail.com>
2024-07-27 10:36:09 +0700

    backup

--- src/lib/components/address.svelte
+++ src/lib/components/address.svelte
@@ -1,51 +1,46 @@
 <script lang="ts">
-	import { fetchAddressSuggestions } from '$lib/hooks/fetchAddressSuggestions';
+	import { fetchAddressSuggestions } from '$lib/hooks/fetchAddressSuggestions.svelte';
 	// 164 Ludlow St New York, NY, USA
-	let address = '';
-	let addressSuggestions = fetchAddressSuggestions(address);
-	let selectedAddress = '';
+	let address = $state('');
+	let addressSuggestions = $derived(fetchAddressSuggestions(address));
 
-	// when address selected from suggestions, suggestions should be cleared to de-render suggestions popup.
-	async function selectAddress(index: number) {
-		if (!addressSuggestions.data) return;
-		selectedAddress = addressSuggestions.data[index];
+	// async function selectAddress(index: number) {
+	// 	// when address selected from suggestions, suggestions should be cleared to de-render suggestions popup.
+	// 	if (!addressSuggestions.data) return;
+	// 	selectedAddress = addressSuggestions.data[index];
+	//
+	// 	return selectedAddress;
+	// }
 
-		return selectedAddress;
-	}
-
-	$inspect(addressSuggestions);
-
-	$inspect({
-		address,
-		isFetching: addressSuggestions.isFetching,
-		data: addressSuggestions.data
-	});
+	$inspect(address, addressSuggestions);
 </script>
 
-<div>
-	<h2 class="text-xl font-medium">Enter your delivery address</h2>
-	{#if !selectedAddress}
-		<input type="text" bind:value={address} placeholder="Enter delivery address" />
-	{/if}
-	{#if addressSuggestions.isFetching || addressSuggestions.isPending}
-		Loading...
-	{/if}
-	{#if addressSuggestions.error}
-		An error has occurred:
-		{addressSuggestions.error.message}
-	{/if}
-	{#if addressSuggestions.isSuccess}
-		<ul>
-			{#each addressSuggestions.data as suggestion, index}
-				<li><button on:click={() => selectAddress(index)}>{suggestion}</button></li>
-			{/each}
-		</ul>
-	{/if}
+<input type="text" bind:value={address} placeholder="Enter delivery address" />
 
-	{#if selectedAddress}
-		<div>
-			<p>{selectedAddress}</p>
-			<button>change</button>
-		</div>
-	{/if}
-</div>
+<!-- <div> -->
+<!-- 	<h2 class="text-xl font-medium">Enter your delivery address</h2> -->
+<!-- 	{#if !selectedAddress} -->
+<!-- 		<input type="text" bind:value={address} placeholder="Enter delivery address" /> -->
+<!-- 	{/if} -->
+<!-- 	{#if addressSuggestions.isFetching || addressSuggestions.isPending} -->
+<!-- 		Loading... -->
+<!-- 	{/if} -->
+<!-- 	{#if addressSuggestions.error} -->
+<!-- 		An error has occurred: -->
+<!-- 		{addressSuggestions.error.message} -->
+<!-- 	{/if} -->
+<!-- 	{#if addressSuggestions.isSuccess} -->
+<!-- 		<ul> -->
+<!-- 			{#each addressSuggestions.data as suggestion, index} -->
+<!-- 				<li><button onclick={() => selectAddress(index)}>{suggestion}</button></li> -->
+<!-- 			{/each} -->
+<!-- 		</ul> -->
+<!-- 	{/if} -->
+<!---->
+<!-- 	{#if selectedAddress} -->
+<!-- 		<div> -->
+<!-- 			<p>{selectedAddress}</p> -->
+<!-- 			<button>change</button> -->
+<!-- 		</div> -->
+<!-- 	{/if} -->
+<!-- </div> -->
